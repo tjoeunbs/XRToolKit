3901a174ff85ad4ebd43ebabf49f9275dada0d65
tjoeunbs <tjoeunbs@naver.com>
2022-08-05 12:53:26 +0900

    Game UI

---
+++ Assets/Scripts/UI/MobCounterUI.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MobCounterUI : MonoBehaviour
+{
+    private int killCount;
+    private int spawnCount;
+
+    private TextMeshProUGUI textUI;
+
+    private void Awake()
+    {
+        textUI = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void UpdateUI()
+    {
+        if (!enabled)
+            return;
+
+        textUI.text = $"Kill/Alive/Spawn\n{killCount}/{spawnCount-killCount}/{spawnCount}";
+    }
+
+    private void OnEnable()
+    {
+        killCount = spawnCount = 0;
+        UpdateUI();
+    }
+
+    public void OnSpawn()
+    {
+        spawnCount++;
+        UpdateUI();
+    }
+
+    public void OnKill()
+    {
+        killCount++;
+        UpdateUI();
+    }
+}
---
+++ Assets/Scripts/UI/SurvivalTimeUI.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SurvivalTimeUI : MonoBehaviour
+{
+    private float startTime;
+
+    private TextMeshProUGUI textUI;
+
+    private void Awake()
+    {
+        textUI = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void OnEnable()
+    {
+        startTime = Time.time;
+    }
+
+    private void Update()
+    {
+        textUI.text = $"Survival Time\n{Time.time - startTime:0.0}s";
+    }
+}
